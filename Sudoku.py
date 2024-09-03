ee81cc3864a73314c68600109437894f1a63fa7b
eric-r-g <eric.rodrig.gomes@gmail.com>
2024-09-03 19:11:43 -0300

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -4,6 +4,7 @@ saida = [
     " ++===+===+===++===+===+===++===+===+===++"
     ]
 lista_de_impres = [0, 1, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 1, 0]
+
 def saida_grade(matriz):
     cont = 0;
     for i in range(21):
@@ -12,5 +13,8 @@ def saida_grade(matriz):
         else:
             print(f"{cont + 1}||{matriz[cont][0]}|{matriz[cont][1]}|{matriz[cont][2]}||{matriz[cont][3]}|{matriz[cont][4]}|{matriz[cont][5]}||{matriz[cont][6]}|{matriz[cont][7]}|{matriz[cont][8]}||{cont + 1}")
             cont += 1
+
+
 matriz = [["   "] * 9] * 9
+matriz[5][5] = " 7 "
 saida_grade(matriz)
