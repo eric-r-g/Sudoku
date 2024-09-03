83a2114054f82c6fe1b2b0cc993afa633fd538fb
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2024-09-03 20:27:30 -0300

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -5,6 +5,13 @@ saida = [
     ]
 lista_de_impres = [0, 1, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 1, 0]
 
+# Função para receber a entrada e retornar o que for necessário
+    # Aqui dentro vai ter a leitura do arquivo provavelmente, talvez seja interessante alguém para isso já que a função é complicada por si só
+
+# Função de iniciar a matriz, recebe o arquivo de pistas
+
+# Substituir o valor da matriz pelos valores recebendo em algum momento aqui
+
 def saida_grade(matriz):
     cont = 0
     for i in range(21):
@@ -14,6 +21,7 @@ def saida_grade(matriz):
             print(f"{cont + 1}||{matriz[cont][0]}|{matriz[cont][1]}|{matriz[cont][2]}||{matriz[cont][3]}|{matriz[cont][4]}|{matriz[cont][5]}||{matriz[cont][6]}|{matriz[cont][7]}|{matriz[cont][8]}||{cont + 1}")
             cont += 1
 
+# Função para verificar se o jogo está valido, recebe a matriz
 
 matriz = [["   " for l in range(9)] for l in range(9)]
 matriz[5][5] = " 7 "
