a4094dcf0545a5d6662b6fee6c58bb38cfce4b40
eric-r-g <eric.rodrig.gomes@gmail.com>
2024-09-03 21:08:00 -0300

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,10 +1,3 @@
-saida = [
-    "    A   B   C    D   E   F    G   H   I",
-    " ++---+---+---++---+---+---++---+---+---++",
-    " ++===+===+===++===+===+===++===+===+===++"
-    ]
-lista_de_impres = [0, 1, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 1, 0]
-
 # Função para receber a entrada e retornar o que for necessário
     # Aqui dentro vai ter a leitura do arquivo provavelmente, talvez seja interessante alguém para isso já que a função é complicada por si só
 
@@ -12,18 +5,32 @@ lista_de_impres = [0, 1, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 1, 0
 
 # Substituir o valor da matriz pelos valores recebendo em algum momento aqui
 
-def saida_grade(matriz):
-    cont = 0
+def saida_grade(mat):
+    # Essa função recebe a matriz e retorna a grade do sudoku já pronta
+    # As variáveis aqui usadas são: 
+        # A mat (a matriz recebida),
+        # fil_padrao (as fileiras que não são modificadas), 
+        # impres (a ordem das fileiras que será impressa),
+        # lin (indica qual das 9 linhas está sendo modificada)
+
+    fil_padrao = [
+    "    A   B   C    D   E   F    G   H   I",
+    " ++---+---+---++---+---+---++---+---+---++",
+    " ++===+===+===++===+===+===++===+===+===++"
+    ]
+    impres = [0, 1, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 2, 3, 1, 3, 1, 3, 1, 0]
+    lin = 0
     for i in range(21):
-        if lista_de_impres[i] < 3:
-            print(saida[lista_de_impres[i]])
+        if impres[i] < 3:
+            print(fil_padrao[impres[i]])
         else:
-            print(f"{cont + 1}||{matriz[cont][0]}|{matriz[cont][1]}|{matriz[cont][2]}||{matriz[cont][3]}|{matriz[cont][4]}|{matriz[cont][5]}||{matriz[cont][6]}|{matriz[cont][7]}|{matriz[cont][8]}||{cont + 1}")
-            cont += 1
+            print("{}|| {} | {} |".format(lin + 1,mat[lin][0],mat[lin][1]),end="")
+            print(" {} || {} | {} |".format(mat[lin][2],mat[lin][3],mat[lin][4]), end="")
+            print(" {} || {} | {} |".format(mat[lin][5],mat[lin][6],mat[lin][7]), end="")
+            print(" {} || {}".format(mat[lin][8], lin + 1))
+            lin += 1
 
 # Função para verificar se o jogo está valido, recebe a matriz
 
-matriz = [["   " for l in range(9)] for l in range(9)]
-matriz[5][5] = " 7 "
+matriz = [[" " for l in range(9)] for l in range(9)]
 saida_grade(matriz)
-print(matriz)
