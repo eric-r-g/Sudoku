9b897a8596e22f05e031bdd80510617684420b49
eric-r-g <eric.rodrig.gomes@gmail.com>
2024-09-15 13:22:28 -0300

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -61,3 +61,25 @@ def verificar_jogada(sudoku, entrada_formatada):
             jogada_dados = [6]
         
     return jogada_dados
+
+# Registra as mensagens de erro baseado em um número correspondente.
+# Alguns eventos envolvem a jogada/ação do usuário e podem ser passadas como parâmetro da função.
+def exibir_erro(codigo, entrada=None):
+
+    if entrada != None:
+        entrada = entrada.replace("\n", "").strip()
+    
+    erros = {
+        1: "a linha ou a coluna está fora do alcance",
+        2: "Você não pode interagir com uma pista.",
+        3: "Você não pode ver as possibilidades de uma posição já preenchida",
+        4: "msg 4",
+        5: "O número precisa ser de 1 a 9",
+        6: f"A jogada {entrada} não é possível",
+        7: f"A jogada {entrada} eh invalida",
+        8: "O número inserido é inválido.",
+        9: "Não existe nada para ser apagado na posição solicitada."
+    }
+    
+    if abs(codigo) in erros:
+        print(erros[codigo])
