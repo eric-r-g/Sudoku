0ed403d315ed1bd54366d4c2ef24a226845791fa
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2024-09-15 12:03:01 -0300

    Implementando controle de mouse para interface
    Adicionei também as ultimas mudanças do programa principal, além de organizar e comentar o código

--- interface.py
+++ interface.py
@@ -2,11 +2,20 @@
 import tkinter as tk
 import Classe as sdk
 
+# https://tkdocs.com/shipman/tkinter.pdf documentação inteira do tkinter em pdf
+# https://tkdocs.com/shipman/index-2.html documentação no site
+
+# Problemas: 
+# Talvez o tamanho das coisas seja pequeno demais, e feio, não sei como melhorar ainda pelos menos
+# Separar os quadrantes talvez vá ser complicado, minha ideia é separar o grid em 9 partes e a cor de fundo é a separação, só teria que ver como posicionar isso com o tk
+
+# Classe herdada do programa principal
 class Sudoku_interface(sdk.Sudoku):
     def __init__(self):
         super().__init__()
         self.grade = [[0 for _ in range(9)] for _ in range(9)]
         self.grade_str = [[tk.StringVar(value="   ") for _ in range(9)] for _ in range(9)]
+        self.celulas_preenchidas = 0
     
     def inserir_numero(self, coluna, linha, numero):
         numero = int(numero)
@@ -16,6 +25,7 @@ class Sudoku_interface(sdk.Sudoku):
         else:
             self.grade[linha][coluna] = numero
             self.grade_str[linha][coluna].set(" "+str(numero))
+            self.celulas_preenchidas += 1
 
     def apagar_numero(self, coluna, linha):
         if self.grade[linha][coluna] == 0:
@@ -25,6 +35,7 @@ class Sudoku_interface(sdk.Sudoku):
         else:
             self.grade[linha][coluna] = 0
             self.grade_str[linha][coluna].set("   ")
+            self.celulas_preenchidas -= 1
     
     def obter_dica(self, coluna, linha):
         n_possiveis = self.verificar_possibilidades(coluna, linha)
@@ -34,36 +45,68 @@ class Sudoku_interface(sdk.Sudoku):
         else:
             output("Número(s) possiveis: " + ', '.join(map(str, n_possiveis)))
 
-# https://tkdocs.com/shipman/tkinter.pdf documentação inteira em pdf
-# https://tkdocs.com/shipman/index-2.html documentação no site
-
-# Problemas: 
-# Talvez o tamanho das coisas seja pequeno demais, e feio, não sei como melhorar ainda pelos menos
-# Separar os quadrantes talvez vá ser complicado, minha ideia é separar o grid em 9 partes e a cor de fundo é a separação, só teria que ver como posicionar isso com o tk
-
-# Coisas pra dar uma olhada:
-# Em geral não seria difícil criar um controle de clique e teclado se a gente conseguir fazer a base da janela: https://tkdocs.com/shipman/events.html
-
 def obter_arquivo(arquivo):
   with open(arquivo, 'r') as file:
     return file.readlines()
-def obter_arquivo_str(arquivo):
-    with open(arquivo, 'r') as file:
-        return file.read()
-    
+
+# Declarar a janela principal, em seguida algumas configurações para ela
+root = tk.Tk() 
+root.title("Sudoku")
+root.configure(background='black')
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+root.minsize(400, 310)
+
+# Variáveis principais do programa
+sudoku = Sudoku_interface()
+qntd_outputs = tk.IntVar(value=0)
+input_text = tk.StringVar()
+file_text = tk.StringVar()
+output_str = tk.StringVar(value="Insira o caminho de um arquivo na entrada inferior")
+
+
+# ========= Criação e configuração dos frames e widgets do programa ========= #
+
+title_frame = tk.Frame(root, bg="blue")
+title_frame.pack(side="top", fill="both")
+tk.Label(title_frame, text="SUDOKU").pack(expand=True)
+
+# Alguns frames usados para posicionar a grade no centro da tela
+sudoku_frame = tk.Frame(root, bg="green")
+sudoku_frame.pack(side="top", fill="both")
+table_frame = tk.Frame(sudoku_frame)
+table_frame.pack(expand=True)
+
+entry_frame = tk.Frame(root, bg="red", )
+entry_frame.pack(side="top", fill="both")
+
+input_entry = tk.Entry(entry_frame, textvariable=input_text)
+tk.Label(entry_frame, text="Insira suas jogadas aqui").pack(side="top")
+input_entry.pack(side="top",expand=True)
+
+file_entry = tk.Entry(entry_frame, textvariable=file_text)
+tk.Label(entry_frame, text="Insira o arquivo de pistas aqui").pack(side="top")
+file_entry.pack(side="top",expand=True)
+
+# Label que mostra os outputs do programa
+tk.Label(entry_frame, textvariable=output_str).pack(side="bottom")
+
+
+# ========= Funções principais do programa ========= #
+
 def exibir_erro(codigo, entrada=None):
 
     if entrada != None:
         entrada = entrada.replace("\n", "").strip()
 
     erros = {
-        1: "msg 1",
+        1: "a linha ou a coluna está fora do alcance",
         2: "Você não pode interagir com uma pista.",
-        3: "msg 3",
+        3: "Você não pode ver as possibilidades de uma posição já preenchida",
         4: "msg 4",
-        5: "msg 5",
+        5: "O número precisa ser de 1 a 9",
         6: f"A jogada {entrada} não é possível",
-        7: "A jogada " + entrada + " eh invalida",
+        7: f"A jogada {entrada} eh invalida",
         8: "O número inserido é inválido.",
         9: "Não existe nada para ser apagado na posição solicitada."
     }
@@ -71,66 +114,17 @@ def exibir_erro(codigo, entrada=None):
     if abs(codigo) in erros:
         output(erros[codigo])
 
-# root é a janela do programa
-root = tk.Tk() 
-root.title("Sudoku")
-root.configure(background='black')
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-sudoku = Sudoku_interface()
-#num_pres_linha = [[False for l in range(9)] for l in range(9)]
-#num_pres_coluna = [[False for l in range(9)] for l in range(9)]
-#num_pres_quadrante = [[False for l in range(9)] for l in range(9)]
-#eh_pista = [[False for l in range(9)] for l in range(9)]
-label_matriz = [[None for l in range(9)] for l in range(9)]
-
-qntd_avisos = tk.IntVar(value=0)
-
-label_frame = tk.Frame(root, bg="blue")
-label_frame.pack(side="top", fill="both")
-tk.Label(label_frame, text="SUDOKU").pack(expand=True)
-
-sudoku_frame = tk.Frame(root, bg="green")
-sudoku_frame.pack(side="top", fill="both")
-table_frame = tk.Frame(sudoku_frame)
-table_frame.pack(expand=True)
-
-input_text = tk.StringVar()
-file_text = tk.StringVar()
-entry_frame = tk.Frame(root, bg="red")
-entry_frame.pack(side="top", fill="both")
-
-input_entry = tk.Entry(entry_frame, textvariable=input_text)
-label_entry = tk.Label(entry_frame, text="Insira suas jogadas aqui").pack(side="top")
-input_entry.pack(side="top",expand=True)
-
-file_entry = tk.Entry(entry_frame, textvariable=file_text)
-label_file = tk.Label(entry_frame, text="Insira o arquivo de pistas aqui").pack(side="top")
-file_entry.pack(side="top",expand=True)
-
-aviso_str = tk.StringVar()
-label_aviso = tk.Label(entry_frame, textvariable=aviso_str).pack(side="bottom")
-
-for coluna in range(9):
-    for linha in range(9):
-        label_matriz[linha][coluna] = tk.Label(table_frame, textvariable=sudoku.grade_str[linha][coluna], borderwidth=1, relief="solid", padx=2, pady=2).grid(column=coluna, row=linha+2, sticky=tk.N)
-# retorna uma matriz de strings normais a partir da matriz de StringVar do tkinter
-#def get_matriz(matriz):
-#    matriz_retorno = [[None for l in range(9)] for l in range(9)]
-#    for coluna in range(9):
-#        for linha in range(9):
-#            matriz_retorno[linha][coluna] = matriz[linha][coluna].get()
-
-# mostrar mensagem
-def output(aviso):
-    if aviso_str.get() != aviso and aviso_str.get() != aviso+f'[{qntd_avisos.get()}]':
-        aviso_str.set(aviso)
-        qntd_avisos.set(0)
+# Mostrar mensagem (Uma função print() mas na tela)
+def output(output):
+    # Sisteminha pra mostrar repetições de output no formato "repetição[1]"
+    if output_str.get() != output and output_str.get() != output+f'[{qntd_outputs.get()}]':
+        output_str.set(output)
+        qntd_outputs.set(0)
     else:
-        qntd_avisos.set(qntd_avisos.get() + 1)
-        aviso_str.set(aviso+f'[{qntd_avisos.get()}]')
+        qntd_outputs.set(qntd_outputs.get() + 1)
+        output_str.set(output+f'[{qntd_outputs.get()}]')
 
+# Exatamente igual a do programa principal, copiada para usar o exibir_erro() daqui
 def validar_entrada(sudoku, entrada):
     if sdk.formatar(entrada) == None:
         exibir_erro(7, entrada)
@@ -138,45 +132,104 @@ def validar_entrada(sudoku, entrada):
     else:
         return sdk.verificar_jogada(sudoku, sdk.formatar(entrada))
 
-def registrar_pistas(evento = None):
+def registrar_pistas(evento):
     try:
         pistas = obter_arquivo(file_text.get())
         qntd_pistas = len(pistas)
         if qntd_pistas >= 1 and qntd_pistas <= 80:
             file_text.set("")
+            invalida = False
             for pista in pistas:
                 pista_form = validar_entrada(sudoku, pista)
-                if pista_form != None and len(pista_form) == 3:
+                if pista_form != None and len(pista_form) == 3 and not invalida:
                     coluna, linha, numero = pista_form
                     sudoku.inserir_numero(coluna, linha, numero)
                     sudoku.pistas[pista_form[1]][pista_form[0]] = True
                 else:
-                    output("Configuração inválida")
+                    invalida = True
+            if invalida:
+                output("Configuração inválida")
+            else:    
+                output('Use a entrada superior ou clique em um espaço')
         else:
             output("Quantidade inválida de pistas: "+str(qntd_pistas))
     except FileNotFoundError as erro:
         output(erro.strerror)
 
-# Função intermediária de inserção
-def inserir_teclado(evento = None):
+# Função intermediária de inserção para o teclado
+def inserir_teclado(evento):
     entrada = input_text.get()
     jogada = validar_entrada(sudoku, entrada)
-    if jogada != None and len(jogada) == 3:
+    if jogada != None and len(jogada) == 3 and not sudoku.finalizado:
         sdk.registrar_acao(sudoku, jogada)
         input_text.set("")
+    elif sudoku.celulas_preenchidas == 81 or sudoku.finalizado:
+        sudoku.finalizado = True
+        output('Parabéns! Você completou o sudoku')
     else:
         exibir_erro(jogada[0], entrada)
 
-def foco_entrada(evento = None):
+# Função intermediária de controle dos eventos de mouse
+def handle_click(evento):
+    widget = evento.widget
+    widget.focus_set()
+
+    # Função intermediária de inserção para o mouse
+    def inserir_click(evento):
+        def processo_insercao(entrada):
+            jogada = validar_entrada(sudoku, entrada)
+            if jogada != None and len(jogada) == 3:
+                sdk.registrar_acao(sudoku, jogada)
+                input_text.set("")
+                print(sudoku.celulas_preenchidas)
+            elif sudoku.celulas_preenchidas == 81 or sudoku.finalizado:
+                sudoku.finalizado = True
+                output('Parabéns! Você completou o sudoku')
+            else:
+                exibir_erro(jogada[0], entrada)
+        digito = evento.keysym
+        if digito != "0" and digito.isnumeric():
+            entrada = f'{chr(widget.coluna+65)},{widget.linha+1}:{digito}'
+            processo_insercao(entrada)
+        elif digito == "0":
+            entrada = f'?{chr(widget.coluna+65)},{widget.linha+1}'
+            processo_insercao(entrada)
+        elif digito == "BackSpace":
+            entrada = f'!{chr(widget.coluna+65)},{widget.linha+1}'
+            processo_insercao(entrada)
+        elif digito == "Escape":
+            output("Jogada cancelada")
+        root.focus()
+        
+
+    output('Insira um digito no seu teclado, Backspace para apagar,\n0 para verificar possibilidades ou Escape para cancelar')
+    widget.bind('<KeyPress>', inserir_click)
+
+# Funções intermediárias de controle dos eventos de teclado
+def foco_entrada(evento):
     root.unbind_all('<Return>')
     root.bind('<Return>', inserir_teclado)
-def foco_file(evento = None):
+def foco_file(evento):
     root.unbind_all('<Return>')
     root.bind('<Return>', registrar_pistas)
 
-input_entry.bind("<FocusIn>", foco_entrada)
-file_entry.bind("<FocusIn>", foco_file)
+
+# ========= Inicialização do programa ========= #
+
+# Criação da grade do sudoku na janela
+for coluna in range(9):
+    for linha in range(9):
+        cell = tk.Label(table_frame, textvariable=sudoku.grade_str[linha][coluna], borderwidth=1, relief="solid", padx=2, pady=2)
+        cell.grid(column=coluna, row=linha+2, sticky=tk.N)
+        cell.linha = linha
+        cell.coluna = coluna
+        cell.conteudo = sudoku.grade_str[linha][coluna]
+        cell.bind('<Button-1>',handle_click)
+
+# Eventos de teclado do programa
+input_entry.bind('<FocusIn>', foco_entrada)
+file_entry.bind('<FocusIn>', foco_file)
 root.bind('<Return>', inserir_teclado)
 
-#Rodar o programa
+# Iniciar o programa
 root.mainloop()
