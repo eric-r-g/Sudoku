41e03b19c514ce96ef9cc7c91965d414fd8fd7dc
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2024-09-16 19:42:50 -0300

    Modo batch e organização de código

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 
 import tkinter as tk
-import Classe as sdk
+from Classe import Sudoku
+from validacao import validar_entrada
+from main import registrar_acao
 
 # https://tkdocs.com/shipman/tkinter.pdf documentação inteira do tkinter em pdf
 # https://tkdocs.com/shipman/index-2.html documentação no site
@@ -10,10 +12,10 @@ import Classe as sdk
 # Separar os quadrantes talvez vá ser complicado, minha ideia é separar o grid em 9 partes e a cor de fundo é a separação, só teria que ver como posicionar isso com o tk
 
 # Classe herdada do programa principal
-class Sudoku_interface(sdk.Sudoku):
+class Sudoku_interface(Sudoku):
     def __init__(self):
         super().__init__()
-        self.grade_str = [[tk.StringVar(value="   ") for _ in range(9)] for _ in range(9)]
+        self.grade_str = [[tk.StringVar(value="    ") for _ in range(9)] for _ in range(9)]
         self.grade_label = [[None for _ in range(9)] for _ in range(9)]
     
     def inserir_numero(self, coluna, linha, numero):
@@ -23,10 +25,10 @@ class Sudoku_interface(sdk.Sudoku):
             exibir_erro(2)
         else:
             self.grade[linha][coluna] = numero
-            self.grade_str[linha][coluna].set(" "+str(numero))
+            self.grade_str[linha][coluna].set(" "+str(numero)+" ")
             self.celulas_preenchidas += 1
-            if sudoku.celulas_preenchidas == 81 or sudoku.finalizado:
-                sudoku.finalizado = True
+            if self.celulas_preenchidas == 81 or self.finalizado:
+                self.finalizado = True
                 output('Parabéns! Você completou o sudoku')
 
     def apagar_numero(self, coluna, linha):
@@ -36,7 +38,7 @@ class Sudoku_interface(sdk.Sudoku):
             exibir_erro(2)
         else:
             self.grade[linha][coluna] = 0
-            self.grade_str[linha][coluna].set("   ")
+            self.grade_str[linha][coluna].set("    ")
             self.celulas_preenchidas -= 1
     
     def obter_dica(self, coluna, linha):
@@ -46,6 +48,15 @@ class Sudoku_interface(sdk.Sudoku):
             output("Não possui números possiveis para essa posição.")
         else:
             output("Número(s) possiveis: " + ', '.join(map(str, n_possiveis)))
+    def clear(self):
+        for coluna in range(9):
+            for linha in range(9):
+                self.grade[linha][coluna] = 0
+                self.pistas[linha][coluna] = False
+                self.grade_str[linha][coluna].set("    ")
+        self.celulas_preenchidas = 0
+        self.finalizado = False
+        output("Jogo resetado")
 
 def obter_arquivo(arquivo):
   with open(arquivo, 'r') as file:
@@ -57,50 +68,17 @@ root.title("Sudoku")
 root.configure(background='black')
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-root.minsize(400, 310)
+root.minsize(600, 310)
 
 # Variáveis principais do programa
 sudoku = Sudoku_interface()
 qntd_outputs = tk.IntVar(value=0)
 input_text = tk.StringVar()
 file_text = tk.StringVar()
+batch_text = tk.StringVar()
 output_str = tk.StringVar(value="Insira o caminho de um arquivo na entrada inferior")
-batch = tk.BooleanVar(False)
-
-# ========= Criação e configuração dos frames e widgets do programa ========= #
-title_frame = tk.Frame(root, bg="#5432a8")
-title_frame.pack(side="top", fill="both")
-tk.Label(title_frame, text="SUDOKU").pack(expand=True)
-
-# Alguns frames usados para posicionar a grade no centro da tela
-sudoku_frame = tk.Frame(root)
-sudoku_frame.pack(side="top", fill="both")
-#table_frame = tk.Frame(sudoku_frame)
-#table_frame.pack()
-logo = tk.PhotoImage(file="images.pgm")
-label_img = tk.Label(sudoku_frame, image=logo)
-label_img.pack(expand=True, fill="both")
-
-entry_frame = tk.Frame(root, bg="#3f2580")
-entry_frame.pack(side="top", fill="both")
-
-options_frame = tk.Frame(sudoku_frame, bg="yellow")
-options_frame.place(relx=0.85, rely=0.05)
-
-tk.Label(options_frame, text="Opções").pack(side="top")
-tk.Checkbutton(options_frame, variable=)
-
-input_entry = tk.Entry(entry_frame, textvariable=input_text)
-tk.Label(entry_frame, text="Insira suas jogadas aqui").pack(side="top")
-input_entry.pack(side="top",expand=True)
-
-file_entry = tk.Entry(entry_frame, textvariable=file_text)
-tk.Label(entry_frame, text="Insira o arquivo de pistas aqui").pack(side="top")
-file_entry.pack(side="top",expand=True)
-
-# Label que mostra os outputs do programa
-tk.Label(entry_frame, textvariable=output_str).pack(side="bottom")
-
+batch = tk.BooleanVar(value=False)
+batch_anim = tk.BooleanVar(value=True)
 
 # ========= Funções principais do programa ========= #
 
@@ -127,6 +105,7 @@ def exibir_erro(codigo, entrada=None):
 # Mostrar mensagem (Uma função print() mas na tela)
 def output(output):
     # Sisteminha pra mostrar repetições de output no formato "repetição[1]"
+    output = str(output)
     if output_str.get() != output and output_str.get() != output+f'[{qntd_outputs.get()}]':
         output_str.set(output)
         qntd_outputs.set(0)
@@ -134,23 +113,14 @@ def output(output):
         qntd_outputs.set(qntd_outputs.get() + 1)
         output_str.set(output+f'[{qntd_outputs.get()}]')
 
-# Exatamente igual a do programa principal, copiada para usar o exibir_erro() daqui
-def validar_entrada(sudoku, entrada):
-    if sdk.formatar(entrada) == None:
-        exibir_erro(7, entrada)
-        return None
-    else:
-        return sdk.verificar_jogada(sudoku, sdk.formatar(entrada))
-
 def registrar_pistas(evento):
     try:
         pistas = obter_arquivo(file_text.get())
         qntd_pistas = len(pistas)
         if qntd_pistas >= 1 and qntd_pistas <= 80:
-            file_text.set("")
             invalida = False
             for pista in pistas:
-                pista_form = sdk.validar_entrada(sudoku, pista)
+                pista_form = validar_entrada(sudoku, pista)
                 coluna, linha, numero = pista_form
                 if pista_form != None and len(pista_form) == 3 and not invalida:
                     sudoku.inserir_numero(coluna, linha, numero)
@@ -166,13 +136,27 @@ def registrar_pistas(evento):
             output("Quantidade inválida de pistas: "+str(qntd_pistas))
     except FileNotFoundError as erro:
         output(erro.strerror)
+    file_text.set("")
+
+def registrar_batch(evento):
+    if batch.get():
+        entradas = obter_arquivo(batch_text.get())
+        tempo = 0
+        for entrada in entradas:
+            tempo+=1
+            jogada = validar_entrada(sudoku, entrada)
+            if jogada != None and len(jogada) == 3:
+                if batch_anim.get(): sudoku.grade_label[jogada[1]][jogada[0]].after(100*tempo, registrar_acao, sudoku, jogada)
+                else: registrar_acao(sudoku, jogada)
+
+    batch_text.set('')
 
 # Função intermediária de inserção para o teclado
 def inserir_teclado(evento):
     entrada = input_text.get()
     jogada = validar_entrada(sudoku, entrada)
     if jogada != None and len(jogada) == 3 and not sudoku.finalizado:
-        sdk.registrar_acao(sudoku, jogada)
+        registrar_acao(sudoku, jogada)
         input_text.set("")
 
     else:
@@ -188,7 +172,7 @@ def handle_click(evento):
         def processo_insercao(entrada):
             jogada = validar_entrada(sudoku, entrada)
             if jogada != None and len(jogada) == 3:
-                sdk.registrar_acao(sudoku, jogada)
+                registrar_acao(sudoku, jogada)
                 input_text.set("")
             elif sudoku.celulas_preenchidas == 81 or sudoku.finalizado:
                 sudoku.finalizado = True
@@ -213,6 +197,18 @@ def handle_click(evento):
     output('Insira um digito no seu teclado, Backspace para apagar,\n0 para verificar possibilidades ou Escape para cancelar')
     widget.bind('<KeyPress>', inserir_click)
 
+def set_batch():
+    if batch.get():
+        batch_entry = tk.Entry(batch_frame, textvariable=batch_text, name="entry")
+        batch_label = tk.Label(batch_frame, text="Insira o arquivo de entrada aqui", name="label")
+        batch_label.pack(side="top")
+        batch_entry.pack(side="top",expand=True)
+        batch_entry.bind('<FocusIn>', foco_batch)
+    else:
+        batch_frame.children['entry'].destroy()
+        batch_frame.children['label'].destroy()
+    batch_text.set('') 
+
 # Funções intermediárias de controle dos eventos de teclado
 def foco_entrada(evento):
     root.unbind_all('<Return>')
@@ -220,28 +216,69 @@ def foco_entrada(evento):
 def foco_file(evento):
     root.unbind_all('<Return>')
     root.bind('<Return>', registrar_pistas)
+def foco_batch(evento):
+    root.unbind_all('<Return>')
+    root.bind('<Return>', registrar_batch)
 
+# ========= Criação e configuração dos frames e widgets do programa ========= #
+title_frame = tk.Frame(root, bg="#5432a8")
+title_frame.pack(side="top", fill="both")
+tk.Label(title_frame, text="SUDOKU").pack(expand=True)
+
+# Alguns frames usados para posicionar a grade no centro da tela
+sudoku_frame = tk.Frame(root, bg="#7e42f5")
+sudoku_frame.pack(side="top", fill="both")
+table_frame = tk.Frame(sudoku_frame, bg="black")
+table_frame.pack()
+
+entry_frame = tk.Frame(root, bg="#3f2580")
+entry_frame.pack(side="top", fill="both")
+
+options_frame = tk.Frame(sudoku_frame, bg="yellow")
+options_frame.place(relx=0.75, rely=0.05)
+
+tk.Label(options_frame, text="Opções").pack(side="top")
+tk.Checkbutton(options_frame, variable=batch, command=set_batch, text="Modo batch").pack(side="top")
+tk.Checkbutton(options_frame, variable=batch_anim, text="Animação do batch").pack(side="top")
+tk.Button(options_frame, command=sudoku.clear, text="Resetar jogo", fg="red").pack(side="top")
+tk.Button(options_frame, command=root.quit, text="Sair", fg="red").pack(side="top")
+
+input_entry = tk.Entry(entry_frame, textvariable=input_text)
+tk.Label(entry_frame, text="Insira suas jogadas aqui").pack(side="top")
+input_entry.pack(side="top",expand=True)
+
+file_entry = tk.Entry(entry_frame, textvariable=file_text)
+tk.Label(entry_frame, text="Insira o arquivo de pistas aqui").pack(side="top")
+file_entry.pack(side="top",expand=True)
+
+batch_frame = tk.Frame(entry_frame, bg="#3f2580")
+batch_frame.pack(side="top")
+
+# Label que mostra os outputs do programa
+tk.Label(entry_frame, textvariable=output_str).pack(side="bottom")
 
 # ========= Inicialização do programa ========= #
 
 # Criação da grade do sudoku na janela
-for coluna in range(9):
-    for linha in range(9):
-        cell = tk.Label(label_img, textvariable=sudoku.grade_str[linha][coluna], borderwidth=1, relief="solid", padx=2, pady=2)
-        cell.grid(column=coluna, row=linha+2, sticky=tk.N)
-        cell.linha = linha
-        cell.coluna = coluna
-        cell.conteudo = sudoku.grade_str[linha][coluna]
-        cell.bind('<Button-1>',handle_click)
-        sudoku.grade_label[linha][coluna] = cell
+def iniciar_matriz():
+    for coluna in range(9):
+        for linha in range(9):
+            cell = tk.Label(table_frame, textvariable=sudoku.grade_str[linha][coluna], borderwidth=1, relief="solid", padx=2, pady=2, font="Bold")
+            cell.grid(column=coluna, row=linha)
+            cell.linha = linha
+            cell.coluna = coluna
+            cell.conteudo = sudoku.grade_str[linha][coluna]
+            cell.bind('<Button-1>',handle_click)
+            sudoku.grade_label[linha][coluna] = cell
 
 # Eventos de teclado do programa
 input_entry.bind('<FocusIn>', foco_entrada)
 file_entry.bind('<FocusIn>', foco_file)
 root.bind('<Return>', inserir_teclado)
 
-def iniciar():
 # Iniciar o programa
+def iniciar():
+    iniciar_matriz()
     root.mainloop()
 iniciar()
 
