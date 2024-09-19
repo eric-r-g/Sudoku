71e4df2fbdefea976a5ce6ed4d41d17233a374a3
eric-r-g <eric.rodrig.gomes@gmail.com>
2024-09-19 09:48:47 -0300

    porque já importando começa o jogo???

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 
 from Classe import Sudoku
 from validacao import validar_entrada, formatar, verificar_jogada, exibir_erro
-from interface import iniciar_interface
+
 
 # Função principal
 def iniciar(modo):
@@ -143,8 +143,8 @@ def registrar_acao(sudoku, acao):
 if len(sys.argv) < 2 or len(sys.argv) > 3:
   print("O numero de parametros enviados para executar o sudoku eh invalido.")
 else:
-    if sys.argv[0] == "interface":
-        iniciar_interface()
+    if sys.argv[1] == "interface":
+        from interface import iniciar_interface
     else:
         modo = len(sys.argv) - 1
         iniciar(modo)
